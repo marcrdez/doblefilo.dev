1e59360d854882994c5d856c55b3cbbd36f313d4
Marc Rodriguez <rodriguezmoreno.marcos@gmail.com>
2022-11-03 14:36:28 +0100

    feat: add NavBar

---
+++ pages/components/NavBar.js
@@ -0,0 +1,20 @@
+import Link from "next/link";
+import styles from "../../styles/NavBar.module.css";
+
+export const NavBar = () => {
+  return (
+    <header className={styles.main}>
+      <nav>
+        <Link href="/" className={styles.link}>
+          Home
+        </Link>
+        <Link href="/blog" className={styles.link}>
+          Blog
+        </Link>
+        <Link href="/contact" className={styles.link}>
+          Contact
+        </Link>
+      </nav>
+    </header>
+  );
+};
