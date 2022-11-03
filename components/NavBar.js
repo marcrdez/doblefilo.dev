eb220b27daa73805a8b27d5dfc493b47fa86c30c
Marc Rodriguez <rodriguezmoreno.marcos@gmail.com>
2022-11-03 14:41:48 +0100

    fix: components folder

--- components/NavBar.js
+++ components/NavBar.js
@@ -1,5 +1,5 @@
 import Link from "next/link";
-import styles from "../../styles/NavBar.module.css";
+import styles from "../styles/NavBar.module.css";
 
 export const NavBar = () => {
   return (
--- pages/contact.js
+++ pages/contact.js
@@ -1,5 +1,5 @@
 import Head from "next/head";
-import { NavBar } from "./components/NavBar";
+import { NavBar } from "../components/NavBar";
 import styles from "../styles/Home.module.css";
 
 export default function Contact() {
