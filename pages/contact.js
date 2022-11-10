e2fe2d4ec52c7755bc6431c932c8b85919ed729b
Marc Rodriguez <rodriguezmoreno.marcos@gmail.com>
2022-11-10 19:32:25 +0100

    fix: Home css changed to Main

--- pages/contact.js
+++ pages/contact.js
@@ -1,6 +1,6 @@
 import Head from "next/head";
 import { NavBar } from "../components/NavBar";
-import styles from "../styles/Home.module.css";
+import styles from "../styles/Main.module.css";
 
 export default function Contact() {
   return (
